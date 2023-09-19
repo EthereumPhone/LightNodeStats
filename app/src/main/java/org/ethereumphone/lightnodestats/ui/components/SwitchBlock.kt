8795d2215656cb4cd6e2e6870af20a274af3f472
emunsi <emunsi123@outlook.com>
2023-09-19 13:37:07 +0200

    Revert "Switchblock fullwidth"
    This reverts commit 07d5a57438eb15e6b578fd100cce1961342c159e.

--- app/src/main/java/org/ethereumphone/lightnodestats/ui/components/SwitchBlock.kt
+++ app/src/main/java/org/ethereumphone/lightnodestats/ui/components/SwitchBlock.kt
@@ -76,7 +76,6 @@ fun SwitchBlock(options: Array<String>, onSelectedOption: (String) -> Unit) {
         }
 
         DropdownMenu(
-            modifier = Modifier.fillMaxWidth(),
             expanded = expanded.value,
             onDismissRequest = { expanded.value = false },
             modifier = Modifier
